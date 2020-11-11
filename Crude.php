5420352736c547db8a39231efdbc303e4d4f060f
Mohammad Salem Noorzai <74237721+Mohammad-Salem@users.noreply.github.com>
2020-11-11 22:01:12 +0430

    MyhomeWork

--- Crude.php
+++ Crude.php
@@ -14,7 +14,36 @@ function config(){
 
   function addStudent($data){
     $connect = config();
-    $sql = "INSERT INTO student(Name,Lastname,Email,Password,chid) VALUES('$data[name]','$data[lastname]','$data[email]','$data[password]')";
+    $sql = "INSERT INTO student(Name,Lastname,Email,Password) VALUES('$data[name]','$data[lastname]','$data[email]','$data[password]')";
+    mysqli_query($connect,$sql);
+  }
+
+  function listStudent(){
+    $connect = config();
+    $sql = "SELECT * FROM student";
+    $row = mysqli_query($connect,$sql);
+    while($res = mysqli_fetch_assoc($row)){
+      $result[] = $res;
+    }
+    return $result;
+  }
+
+  function deleteStudent($id){
+    $connect = config();
+    $sql = "DELETE FROM student WHERE id='$id'";
+    $row = mysqli_query($connect,$sql);
+  }
+
+  function showEditStudent($id){
+    $connect = config();
+    $sql = "SELECT * FROM student WHERE id='$id'";
+    $row = mysqli_query($connect,$sql);
+    $res = mysqli_fetch_assoc($row);
+    return $res;
+  }
+  function editStudent($data,$id){
+    $connect = config();
+    $sql = "UPDATE student SET Name='$data[name]',Lastname='$data[lastname]',Email='$data[email]',Password='$data[password]' WHERE id='$id'";
     mysqli_query($connect,$sql);
   }
 
